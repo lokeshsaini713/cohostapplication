0ddb281dad5664b0598e2e9320bd053c0ffa0714
Lokesh <Lokeshsaini@>
2025-12-24 14:04:22 +0530

    temp commit

---
+++ Data/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        // Add DbSet for other tables
+
+        // Optional: OnModelCreating for custom mapping
+    }
+}
---
+++ Shared/Model/UserDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Model
+{
+   
+        public class UserDetail
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            // Add other properties matching your table columns
+        }
+}
